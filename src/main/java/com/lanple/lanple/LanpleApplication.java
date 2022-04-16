b3d334bc10e9589b8c3ca382a2159e644256fb71
남선기 <sunba@namseongiui-MacBookAir.local>
2022-04-16 12:56:05 +0900

    INIT : 프로젝트 초기화

---
+++ src/main/java/com/lanple/lanple/LanpleApplication.java
@@ -0,0 +1,13 @@
+package com.lanple.lanple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanple/lanple/LanpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanple.lanple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
